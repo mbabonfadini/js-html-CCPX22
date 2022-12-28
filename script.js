2e700433c822743bb8ce6e4c07f3de260c900e10
mbabonfadini <mbabonfadini@gmail.com>
2022-12-27 21:21:50 -0300

    finalização do projeto com seleção de ingressos e finalização do botão

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const day = document.getElementById('dias')
 const hora = document.getElementById('horas')
 const minuto = document.getElementById('minutos')
 const segundo = document.getElementById('segundos')
-
+const ingressos = []
 const evento = "03 Dec 2023"
 
 function countDown() {
@@ -28,5 +28,73 @@ function corrige(tempo){
     return tempo < 10 ? `0${tempo}` : tempo
 }
 
+function borda(elemento){
+    let card = document.querySelector(elemento)
+    card.classList.toggle('cardHighLight')
+}
+
+// function checkKeyboardCode(){
+//     document.addEventListener("keydown", (e) => {
+//         var name = e.key
+//         var code = e.code
+//         alert(`Key pressed: ${name}, \r\n Key code: ${code}`)
+// },false) }
+
+function addKeyboardEventListeners(){
+    document.addEventListener("keydown", (e) => {
+        var ingresso1 = document.getElementById("quinta")
+        var ingresso2 = document.getElementById("sexta")
+        var ingresso3 = document.getElementById("sabado")
+        var ingresso4 = document.getElementById("domingo")
+
+        var code = e.code
+        if (code == 'Digit1' || code == 'Numpad1'){
+            ingresso1.classList.toggle('cardHighLight')
+            ingresso2.classList.remove('cardHighLight')
+            ingresso3.classList.remove('cardHighLight')
+            ingresso4.classList.remove('cardHighLight')
+        }
+        else if (code == 'Digit2' || code == 'Numpad2'){
+            ingresso1.classList.remove('cardHighLight')
+            ingresso2.classList.toggle('cardHighLight')
+            ingresso3.classList.remove('cardHighLight')
+            ingresso4.classList.remove('cardHighLight')
+        }
+        else if (code == 'Digit3' || code == 'Numpad3'){
+            ingresso1.classList.remove('cardHighLight')
+            ingresso2.classList.remove('cardHighLight')
+            ingresso3.classList.toggle('cardHighLight')
+            ingresso4.classList.remove('cardHighLight')
+        }
+        else if (code == 'Digit4' || code == 'Numpad4'){
+            ingresso1.classList.remove('cardHighLight')
+            ingresso2.classList.remove('cardHighLight')
+            ingresso3.classList.remove('cardHighLight')
+            ingresso4.classList.toggle('cardHighLight')
+        }
+        else {
+            ingresso1.classList.remove('cardHighLight')
+            ingresso2.classList.remove('cardHighLight')
+            ingresso3.classList.remove('cardHighLight')
+            ingresso4.classList.remove('cardHighLight')
+            
+        }}, false);
+}
+
+function selectedCard(elemento){
+    let selecao = document.querySelector(elemento)
+    selecao.classList.toggle('cardSelected')
+    if(ingressos.includes(elemento)) ingressos.pop(elemento)
+    else ingressos.push(elemento)
+}
+
+function showIngressos(){
+    if(ingressos.length > 0)
+        alert(`Ingressos selecionados ${ingressos}`)
+    else alert('Nenhum ingresso selecionado')
+
+}
+addKeyboardEventListeners()
+// checkKeyboardCode()
 countDown()
 setInterval(countDown, 1000)
